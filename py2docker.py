02d1b5ec03b41a130eb2ebd5da336a62ae1c847e
shirou10086 <jinlid@uci.edu>
2023-11-27 05:14:49 +0800

    update reprozip unpack

--- py2docker.py
+++ py2docker.py
@@ -1,6 +1,7 @@
 import subprocess
 import os
 import glob
+import shutil
 
 def run_command(command):
     try:
@@ -10,23 +11,30 @@ def run_command(command):
         print(f"Error executing {command}: {e.stderr}")
         raise
 
-def reprozip_pack_unpack(script_path):
+def reprozip_pack_unpack(script_path, env_dir):
+    # 确保环境目录存在
+    if not os.path.exists(env_dir):
+        os.makedirs(env_dir)
+
     # ReproZip 打包
     pack_command = f"reprozip trace python {script_path}"
     run_command(pack_command)
 
     # ReproZip 打包文件名
-    rpz_file = os.path.basename(script_path).split('.')[0] + '.rpz'
+    rpz_file = os.path.join(env_dir, os.path.basename(script_path).split('.')[0] + '.rpz')
 
     pack_command = f"reprozip pack {rpz_file}"
     run_command(pack_command)
 
     # ReproZip 解包为 Docker 镜像
-    unpack_command = f"reprozip docker {rpz_file}"
+    docker_dir = os.path.join(env_dir, 'docker')
+    unpack_command = f"reprounzip docker setup {rpz_file} {docker_dir}"
     run_command(unpack_command)
 
 if __name__ == "__main__":
     output_dir = './example/output'
     for script_path in glob.glob(os.path.join(output_dir, 'cell_*.py')):
-        reprozip_pack_unpack(script_path)
-        print("cell_*.py complete")
+        base_name = os.path.basename(script_path).split('.')[0]
+        env_dir = os.path.join(output_dir, base_name + '_env')
+        reprozip_pack_unpack(script_path, env_dir)
+        print(f"{base_name} complete")
