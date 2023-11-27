9eb766edbf2fe8fdcb03c0787c2dbe39ce0721f2
Jinli Duan <jinlid@uci.edu>
2023-11-27 01:35:47 -0500

    u

--- py2docker.py
+++ py2docker.py
@@ -1,32 +1,30 @@
-import subprocess
 import os
-import glob
+import subprocess
 
-def run_command(command):
-    try:
-        result = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        print(result.stdout)
-    except subprocess.CalledProcessError as e:
-        print(f"Error executing {command}: {e.stderr}")
-        raise
+def create_dockerfile(file_name, requirements_path, dockerfiles_path):
+    dockerfile_content = f'''
+    FROM python:3.8
+    WORKDIR /app
+    COPY {file_name} /app
+    COPY {requirements_path} /app
+    RUN pip install -r requirements.txt
+    CMD ["python", "/app/{file_name}"]
+    '''
+    dockerfile_path = os.path.join(dockerfiles_path, f"Dockerfile_{file_name.split('.')[0]}")
+    with open(dockerfile_path, 'w') as file:
+        file.write(dockerfile_content)
+    return dockerfile_path
 
-def reprozip_pack_unpack(script_path):
-    # ReproZip 打包
-    pack_command = f"reprozip trace python {script_path}"
-    run_command(pack_command)
+def build_docker_image(dockerfile_path, image_tag):
+    subprocess.run(["docker", "build", "-f", dockerfile_path, "-t", image_tag, "."], check=True)
 
-    # ReproZip 打包文件名
-    rpz_file = os.path.basename(script_path).split('.')[0] + '.rpz'
+output_dir = './example/output'
+requirements_path = os.path.join(output_dir, 'requirements.txt')
+dockerfiles_path = os.path.join(output_dir, 'docker')
 
-    pack_command = f"reprozip pack {rpz_file}"
-    run_command(pack_command)
+os.makedirs(dockerfiles_path, exist_ok=True)
 
-    # ReproZip 解包为 Docker 镜像
-    unpack_command = f"reprozip docker {rpz_file}"
-    run_command(unpack_command)
-
-if __name__ == "__main__":
-    output_dir = './example/output'
-    for script_path in glob.glob(os.path.join(output_dir, 'cell_*.py')):
-        reprozip_pack_unpack(script_path)
-        print("cell_*.py complete")
+for file in os.listdir(output_dir):
+    if file.endswith('.py'):
+        dockerfile_path = create_dockerfile(file, requirements_path, dockerfiles_path)
+        build_docker_image(dockerfile_path, f"cell_{file.split('.')[0]}")
