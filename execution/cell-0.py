4c8b806a11e3d3439fade3d41001facd2f6208d6
Jinli Duan <43664231+shirou10086@users.noreply.github.com>
2024-03-26 16:49:00 -0400

    align github stats

---
+++ execution/cell-0.py
@@ -0,0 +1,18 @@
+# FETCH CODE START
+import ResultsHub as rh# FETCH CODE END
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from io import StringIO
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from io import StringIO
+
+
+# SUBMIT CODE START
+submission = rh.ResultsHubSubmission(cell_number=0, host='results-hub-service.default.svc.cluster.local')
+submission.submit()
+print('Submission Success for cell 0.')
+# SUBMIT CODE END
---
+++ execution/cell-2.py
@@ -0,0 +1,24 @@
+# FETCH CODE START
+import ResultsHub as rhdata = rh.fetchVarResult('data', varAncestorCell=1, host='results-hub-service.default.svc.cluster.local')
+# FETCH CODE END
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from io import StringIO
+
+#read and process
+iris_data = pd.read_csv(StringIO(data))
+
+iris_data = iris_data.dropna()
+
+
+print(iris_data.describe())
+
+
+
+# SUBMIT CODE START
+submission = rh.ResultsHubSubmission(cell_number=2, host='results-hub-service.default.svc.cluster.local')
+submission.addVar('iris_data', locals().get('iris_data', None))
+submission.submit()
+print('Submission Success for cell 2.')
+# SUBMIT CODE END
