8b23f3bbffdc332b40c50da793f5cc694c9a049d
Yizhuo Liang <yizhuo.liang.hello@gmail.com>
2024-03-31 10:57:28 -0400

    add config

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
-import sys
+# Standard packages
+import json
 import os
+import re
 import subprocess
-from splitnotebook import process_notebook
-from dependency import add_code_to_all_files
-from py2docker import create_dockerfile, build_docker_image
-from deploymentUtils import *
-import py2docker
-import importlib.util
+import sys
 import time
 from concurrent.futures import ThreadPoolExecutor, as_completed
-import re
+
+# J2K packages
+from dependency import add_code_to_all_files
+import py2docker
+from py2docker import build_docker_image, create_dockerfile
+from deploymentUtils import *
+from splitnotebook import process_notebook
 
 '''
-This file packs all functions in the project
+This file provide a unified user interface
 '''
+def load_config(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
 
 def push_to_docker_hub(image_tag):
     """
@@ -25,6 +31,7 @@ def push_to_docker_hub(image_tag):
         print(f"Successfully pushed {image_tag} to Docker Hub.")
     except subprocess.CalledProcessError as e:
         print(f"Failed to push {image_tag} to Docker Hub: {e}")
+
 def dockerize_and_push(filename, dockerfiles_path, python_version, output_dir, dockerhub_username, dockerhub_repository):
     """
     packs the functions for create Dockerfile, build image, and push to Docker Hub.
@@ -36,7 +43,7 @@ def dockerize_and_push(filename, dockerfiles_path, python_version, output_dir, d
     push_to_docker_hub(image_name_tag)
     return (f"{dockerhub_username}/{dockerhub_repository}", filename.split('.')[0])
 
-def main(notebook_path, output_dir, dockerhub_username, dockerhub_repository, image_list_path):
+def main(skip_dockerization, notebook_path, output_dir, dockerhub_username, dockerhub_repository, image_list_path, n_docker_worker):
 
     # STEP 0: Check if we can skip the codegen & dockerization phase
     # also prepare the list of image:tag to be deployed
@@ -65,7 +72,7 @@ def main(notebook_path, output_dir, dockerhub_username, dockerhub_repository, im
         python_version = py2docker.get_python_version()
         os.makedirs(dockerfiles_path, exist_ok=True)
 
-        with ThreadPoolExecutor(max_workers=4) as executor:  # max_worker默认设置为4
+        with ThreadPoolExecutor(max_workers=4) as executor:
             futures = [executor.submit(dockerize_and_push, filename, dockerfiles_path, python_version, output_dir, dockerhub_username, dockerhub_repository)
                        for filename in os.listdir(output_dir)
                        if re.match(r'cell\d+\.py$', filename)]  # 更新匹配逻辑
@@ -83,14 +90,15 @@ def main(notebook_path, output_dir, dockerhub_username, dockerhub_repository, im
     #     cleanup_info = open(cleanup_info_filename, 'a')
 
     # STEP 5: Deploy J2K's control plane: PV, PVC, and ResultsHub
-    # create pv
-    node_name = "node0.net-test.rdma-prefetch-pg0.wisc.cloudlab.us" # NOTE: REPLACE NODE_NAME EVERYTIME CHANGE THE MACHINE!
-    local_path = "/data/my-pv"
-    pv_name = "local-pv"
-    storage_size = "1Gi" # Adjust the size as needed
-    pvc_name = "local-pvc-for-rh"
-    namespace = "default" # for ResultsHub and jobs
+    j2k_config = load_config('J2K_CONFIG.json')
+    node_name = j2k_config['results-hub']['master-node-name']
+    local_path = j2k_config['results-hub']['local-pv-path']
+    pv_name = j2k_config['results-hub']['pv-name']
+    storage_size = j2k_config['results-hub']['storage-size']
+    pvc_name = j2k_config['results-hub']['pvc-name']
+    namespace = j2k_config['results-hub']['namespace']
 
+    # create pv
     create_local_pv(node_name, local_path, pv_name, storage_size)
 
     # create pvc
@@ -108,8 +116,13 @@ def main(notebook_path, output_dir, dockerhub_username, dockerhub_repository, im
 if __name__ == '__main__':
     skip_dockerization = True if len(sys.argv) > 1 and sys.argv[1] == "skip" else False
     notebook_path = sys.argv[2] if len(sys.argv) > 2 else './example/iris.ipynb'
-    output_dir = sys.argv[3] if len(sys.argv) > 3 else './execution'
-    dockerhub_username = sys.argv[4] if len(sys.argv) > 4 else "shirou10086"  # Default Docker Hub username
-    dockerhub_repository = sys.argv[5] if len(sys.argv) > 5 else "jup2kub"  # Default Docker Hub repository
+    
+    # Parse the configuration file
+    j2k_config = load_config('J2K_CONFIG.json')
+    output_dir = j2k_config['execution']['output-directory']
+    dockerhub_username = j2k_config['execution']['dockerhub-username']
+    dockerhub_repository = j2k_config['execution']['dockerhub-repo-name']
+    image_list_filename = j2k_config['execution']['image-list-filename']
+    n_docker_worker = j2k_config['execution']['n-docker-worker']
 
-    main(notebook_path, output_dir, dockerhub_username, dockerhub_repository, os.path.join(output_dir, "image_list.txt"))
+    main(skip_dockerization, notebook_path, output_dir, dockerhub_username, dockerhub_repository, os.path.join(output_dir, image_list_filename), n_docker_worker)
