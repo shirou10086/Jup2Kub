875698b30c9a7c8a741ff6f78a5cc638ac0a9ee5
Jinli Duan <43664231+shirou10086@users.noreply.github.com>
2024-03-31 15:39:45 -0400

    fixed dependency issue

--- execution/cell1.py
+++ execution/cell1.py
@@ -2,22 +2,14 @@
 import ResultsHub as rh
 # FETCH CODE END
 
-import os
 
-# Part 1: Reading a text file
-filename = 'example.txt'  # Assuming the file is named example.txt
-with open(filename, 'r') as file:
-    content = file.read()
-    print("Content of example.txt:")
-    print(content)
-
-
+import pandas as pd
+import matplotlib.pyplot as plt
+from io import StringIO
 
 
 # SUBMIT CODE START
 submission = rh.ResultsHubSubmission(cell_number=1, host='results-hub-service.default.svc.cluster.local')
-submission.addVar('filename', locals().get('filename', None))
-submission.addVar('content', locals().get('content', None))
 submission.submit()
 print('Submission Success for cell 1.')
 # SUBMIT CODE END
