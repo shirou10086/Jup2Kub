971adcb0100888cb2877e876c3da882f7313b036
Yizhuo Liang <yizhuo.liang.hello@gmail.com>
2024-05-20 09:08:03 -0400

    update splitnotebook

--- splitnotebook.py
+++ splitnotebook.py
@@ -5,80 +5,33 @@ import subprocess
 import sys
 import shutil
 import glob
-'''
-This file split the notebook and keep track of the imported modules
-'''
-def extract_imports_from_code(code):
-    tree = ast.parse(code)
-    top_level_imports = set()
-    for node in ast.walk(tree):
-        if isinstance(node, ast.Import):
-            for name in node.names:
-                top_level_imports.add(name.name.split('.')[0])
-        elif isinstance(node, ast.ImportFrom):
-            if node.module is not None:
-                top_level_imports.add(node.module.split('.')[0])
-    return top_level_imports
-
 
 def is_standard_library(module_name):
+    import sys
     return module_name in sys.stdlib_module_names
 
-def extract_imports_and_files(notebook):
-    imports = set()
-    files = set()
-
-    for cell in notebook.cells:
-        if cell.cell_type == 'code':
-            cell_imports = extract_imports_from_code(cell.source)
-            imports.update(cell_imports)
-
-            for line in cell.source.split('\n'):
-                if 'open(' in line or 'read_csv(' in line or 'read_excel(' in line:
-                    file_path = line.split('(')[1].split(')')[0].replace("'", "").replace('"', '').strip()
-                    if os.path.isfile(file_path):
-                        files.add(file_path)
-
-    return imports, files
-
-def filter_non_standard_libraries(imports):
-    filtered_imports = {imp for imp in imports if not is_standard_library(imp)}
-    return filtered_imports
-def save_cells_to_files(notebook, output_directory, all_imports):
-    os.makedirs(output_directory, exist_ok=True)
-
-    valid_cell_index = 1
-    for cell in notebook.cells:
-        if cell.cell_type == 'code' and cell.source.strip():
-            cell_file_path = os.path.join(output_directory, f"cell{valid_cell_index}.py")
-
-            # exclude the imports that already in the cell
-            imports_to_add = [imp for imp in all_imports if imp not in cell.source]
-
-            with open(cell_file_path, 'w', encoding='utf-8') as cell_file:
-                # if not included, include there
-                if imports_to_add:
-                    cell_file.write('\n'.join(imports_to_add) + '\n\n')
-                cell_file.write(cell.source)  #write cell from origin notebook
-
-            valid_cell_index += 1
-
-
-
-
-def extract_full_import_statements(code):
-    """
-    Extracts and returns a set of full import statements from a given piece of code.
-    """
+def extract_imports_from_code_python(code):
     tree = ast.parse(code)
-    import_statements = set()
-
+    imports = set()
     for node in ast.walk(tree):
-        if isinstance(node, (ast.Import, ast.ImportFrom)):
-            # Convert the node back to a string of code
-            import_statements.add(ast.unparse(node))
+        if isinstance(node, ast.Import):
+            imports.update([name.name.split('.')[0] for name in node.names])
+        elif isinstance(node, ast.ImportFrom):
+            if node.module:
+                imports.add(node.module.split('.')[0])
+    return imports
 
-    return import_statements
+def extract_imports_from_code_r(code):
+    imports = set()
+    lines = code.split('\n')
+    for line in lines:
+        line = line.strip()
+        if line.startswith('library('):
+            imports.add(line.split('library(')[1].split(')')[0].strip().replace("'", "").replace('"', ''))
+        elif line.startswith('require('):
+            imports.add(line.split('require(')[1].split(')')[0].strip().replace("'", "").replace('"', ''))
+    return imports
+
 
 def save_requirements(dependencies, output_directory):
     # Ensure these libraries are always included
@@ -91,29 +44,65 @@ def save_requirements(dependencies, output_directory):
         file.write(requirements_content)
     print(f"Requirements saved to {requirements_path}")
 
+def save_r_packages(packages, output_directory, filename='install_packages.R'):
+    requiredpackages = []
+
+    # this is a list of required packages for reticulate
+    for pkg in requiredpackages:
+        packages.add(pkg)
+
+    path = os.path.join(output_directory, filename)
+    with open(path, 'w', encoding='utf-8') as file:
+        for pkg in packages:
+            file.write(f'install.packages("{pkg}", repos="http://cran.rstudio.com/")\n')
+        file.write('''
+# set python path
+library(reticulate)
+use_virtualenv("/root/.virtualenvs/r-reticulate", required = TRUE)
+''')
+    print(f"Saved R packages installation script to {path}")
 def copy_resultshub_files(source_directory, target_directory):
     os.makedirs(target_directory, exist_ok=True)
     for file_path in glob.glob(os.path.join(source_directory, '*')):
         target_file_path = os.path.join(target_directory, os.path.basename(file_path))
         shutil.copy2(file_path, target_file_path)
     print(f"All files from {source_directory} have been copied to {target_directory}")
-
 def process_notebook(notebook_path, output_directory):
-    with open(notebook_path, 'r', encoding='utf-8') as f:
-        nb = nbformat.read(f, as_version=4)
+    with open(notebook_path, 'r', encoding='utf-8') as file:
+        notebook = nbformat.read(file, as_version=4)
 
-    all_imports_statements = set()
-    all_imports_names = set()
+    os.makedirs(output_directory, exist_ok=True)
 
-    for cell in nb.cells:
-        if cell.cell_type == 'code':
-            cell_imports_statements = extract_full_import_statements(cell.source)
-            all_imports_statements.update(cell_imports_statements)
+    python_imports = set()
+    r_packages = set()
+    cell_index = 1
 
-            cell_imports_names = extract_imports_from_code(cell.source)
-            all_imports_names.update(cell_imports_names)
+    for cell in notebook['cells']:
+        if cell['cell_type'] == 'code' and cell['source'].strip():
+            is_r_cell = cell['source'].strip().split('\n')[0].startswith('#R')
+            file_extension = '.R' if is_r_cell else '.py'
+            filename = f"cell{cell_index}{file_extension}"
+            cell_path = os.path.join(output_directory, filename)
 
-    dependencies = filter_non_standard_libraries(all_imports_names)
-    save_cells_to_files(nb, output_directory, all_imports_statements)
-    save_requirements(dependencies, output_directory)
+            with open(cell_path, 'w', encoding='utf-8') as cell_file:
+                cell_file.write(cell['source'])
+
+            if is_r_cell:
+                r_packages.update(extract_imports_from_code_r(cell['source']))
+
+            else:
+                python_imports.update(extract_imports_from_code_python(cell['source']))
+
+            print(f"Saved {filename}")
+            cell_index += 1
+
+    save_requirements(python_imports, output_directory)
+    save_r_packages(r_packages, output_directory)
     copy_resultshub_files('./resultshub_python_client', output_directory)
+
+
+
+# Example usage
+notebook_path = './example/Rexample.ipynb'
+output_directory = 'execution'
+process_notebook(notebook_path, output_directory)
